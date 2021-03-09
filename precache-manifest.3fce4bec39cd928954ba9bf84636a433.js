78a62c641046349b794b99fc111432f22f047c37
Abdullah <abdullah.phantum@gmail.com>
2021-03-09 17:24:19 +0500

    gh-pages

---
+++ precache-manifest.3fce4bec39cd928954ba9bf84636a433.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64d470b28c5534dd98b",
+    "url": "/AIR-Urdu-Service/css/app.6ade7bcb.css"
+  },
+  {
+    "revision": "178406d3c61e53d1e839",
+    "url": "/AIR-Urdu-Service/css/chunk-vendors.46b71172.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/AIR-Urdu-Service/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "efcfd6ae5926d3f47d3b521c18938584",
+    "url": "/AIR-Urdu-Service/index.html"
+  },
+  {
+    "revision": "b8c663af3a30acce0651",
+    "url": "/AIR-Urdu-Service/js/about.4b91c407.js"
+  },
+  {
+    "revision": "d64d470b28c5534dd98b",
+    "url": "/AIR-Urdu-Service/js/app.c76034e9.js"
+  },
+  {
+    "revision": "178406d3c61e53d1e839",
+    "url": "/AIR-Urdu-Service/js/chunk-vendors.b9f69fa6.js"
+  },
+  {
+    "revision": "e2c5ac47c4fe5f00f9575b13cf57a97a",
+    "url": "/AIR-Urdu-Service/js/playerjs.js"
+  },
+  {
+    "revision": "86297a606d21384d34bedf1dc719150a",
+    "url": "/AIR-Urdu-Service/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/AIR-Urdu-Service/robots.txt"
+  }
+]);
